f9d4a1d9023b0b03418a5ac6d795ba8489c03f6b
Rob Halff <rob.halff@gmail.com>
2016-03-22 03:50:45 +0100

    update

--- editor.js
+++ editor.js
@@ -14,19 +14,19 @@ module.exports = {
   },
   ports: {
     input: {
-      "element": {
+      element: {
         title: "Element",
         description: "Element",
         type: "HTMLElement"
       },
       "in": {
         title: "Json",
-        description: "JSON Object",
         type: "object",
+        description: "JSON Object",
         async: true,
-        fn: function __IN__(data, x, source, state, input, output) {
+        fn: function __IN__(data, x, source, state, input, output, json_editor) {
           var r = function() {
-            state.in = data;
+            state.in = $.in;
             if (state.jsonEditor) {
               state.jsonEditor.setValue(state.in);
             }
@@ -40,22 +40,24 @@ module.exports = {
       schema: {
         title: "Schema",
         type: "object",
-        async: true,
         description: "A valid JSON Schema to use for the editor. Version 3 and Version 4 of the draft specification are supported.",
-        fn: function __SCHEMA__(data, x, source, state, input, output) {
+        async: true,
+        fn: function __SCHEMA__(data, x, source, state, input, output, json_editor) {
           var r = function() {
-            state.schema = input.options.schema = input.schema;
+            state.schema = $.options.schema = $.schema;
             if (state.jsonEditor) {
+              state.jsonEditor.off('change', state.changeHandler);
               state.jsonEditor.destroy();
             }
-            state.jsonEditor = new json_editor(input.options);
+            state.jsonEditor = new json_editor($.element, $.options);
             // problem if state.in is not about this schema..
-            if (state.in) {
-              state.jsonEditor.setValue(state.in);
-            }
-            state.jsonEditor.on('change', function() {
+            state.jsonEditor.on('ready', function() {
+              if (state.in) {
+                state.jsonEditor.setValue(state.in);
+              }
+              state.jsonEditor.on('change', state.changeHandler);
               output({
-                out: state.jsonEditor.getValue()
+                editor: $.create(state.jsonEditor)
               });
             });
           }.call(this);
@@ -69,7 +71,7 @@ module.exports = {
         title: "Enable",
         description: "Enable",
         async: true,
-        fn: function __ENABLE__(data, x, source, state, input, output) {
+        fn: function __ENABLE__(data, x, source, state, input, output, json_editor) {
           var r = function() {
             if (state.jsonEditor) {
               state.jsonEditor.enable();
@@ -85,7 +87,7 @@ module.exports = {
         title: "Disable",
         description: "Disable",
         async: true,
-        fn: function __DISABLE__(data, x, source, state, input, output) {
+        fn: function __DISABLE__(data, x, source, state, input, output, json_editor) {
           var r = function() {
             if (state.jsonEditor) {
               state.jsonEditor.disable();
@@ -104,81 +106,126 @@ module.exports = {
           ajax: {
             title: "Ajax",
             description: "If true, JSON Editor will load external URLs in $ref via ajax.",
+            type: "boolean",
             "default": false
           },
           disable_array_add: {
             title: "Disable array add",
             description: "If true, remove all `add row` buttons from arrays.",
+            type: "boolean",
             "default": false
           },
           disable_array_delete: {
             title: "Disable array delete",
             description: "If true, remove all `delete row` buttons from arrays.",
+            type: "boolean",
             "default": false
           },
           disable_array_reorder: {
             title: "Disable array reorder",
             description: "If true, remove all `move up` and `move down` buttons from arrays.",
+            type: "boolean",
             "default": false
           },
           disable_collapse: {
             title: "Disable collapse",
             description: "If true, remove all collapse buttons from objects and arrays.",
+            type: "boolean",
             "default": false
           },
           disable_edit_json: {
             title: "Disable edit json",
             description: "If true, remove all Edit JSON buttons from objects.",
+            type: "boolean",
             "default": false
           },
           disable_properties: {
             title: "Disable properties",
             description: "If true, remove all Edit Properties buttons from objects.",
+            type: "boolean",
             "default": false
           },
           form_name_root: {
             title: "Form name root",
             description: "The first part of the `name` attribute of form inputs in the editor. An full example name is `root[person][name]` where `root` is the form_name_root.",
+            type: "boolean",
             "default": "root"
           },
           iconlib: {
             title: "Iconlib",
             description: "The icon library to use for the editor. See the CSS Integration section below for more info.",
+            type: "string",
+            "enum": ["bootstrap3",
+              "bootstrap2",
+              "foundation3",
+              "foundation2",
+              "jqueryui",
+              "fontawesome4",
+              "fontawesome3"
+            ],
             "default": null
           },
           no_additional_properties: {
             title: "No additional properties",
             description: "If true, objects can only contain properties defined with the properties keyword.",
+            type: "boolean",
             "default": false
           },
           refs: {
             title: "Refs",
             description: "An object containing schema definitions for URLs. Allows you to pre-define external schemas.",
+            type: "object",
             "default": {}
           },
           required_by_default: {
             title: "Required by default",
             description: "If true, all schemas that don't explicitly set the required property will be required.",
+            type: "boolean",
             "default": false
           },
           show_errors: {
             title: "Show errors",
             description: "When to show validation errors in the UI. Valid values are interaction, change, always, and never.",
+            type: "string",
+            "enum": ["interaction",
+              "change",
+              "always",
+              "never"
+            ],
             "default": "interaction"
           },
           startval: {
             title: "Start value",
             description: " Seed the editor with an initial value. This should be valid against the editor's schema.",
+            type: "any",
             "default": null
           },
           template: {
             title: "Template",
             description: "The JS template engine to use. See the Templates and Variables section below for more info.",
+            type: "string",
+            "enum": ["default",
+              "ejs",
+              "hogan",
+              "markup",
+              "mustache",
+              "swig",
+              "underscore"
+            ],
             "default": "default"
           },
           theme: {
             title: "Theme",
             description: "The CSS theme to use. See the CSS Integration section below for more info.",
+            type: "string",
+            "enum": ["html",
+              "bootstrap3",
+              "bootstrap2",
+              "foundation5",
+              "foundation4",
+              "foundation3",
+              "jqueryui"
+            ],
             "default": "html"
           }
         }
@@ -188,8 +235,19 @@ module.exports = {
       out: {
         title: "out",
         type: "string"
+      },
+      editor: {
+        title: "Editor",
+        type: "JSONEditor"
       }
     }
   },
-  state: {}
+  state: {
+    jsonEditor: null,
+    changeHandler: function() {
+      output({
+        out: $.clone('in', state.jsonEditor.getValue())
+      });
+    }
+  }
 }
---
+++ validate.js
@@ -0,0 +1,73 @@
+module.exports = {
+  name: "validate",
+  ns: "json-editor",
+  title: "JSON Editor Validate",
+  description: "JSON Editor Validate",
+  async: true,
+  phrases: {
+    active: "Validating form"
+  },
+  ports: {
+    input: {
+      "in": {
+        title: "Json",
+        type: "object",
+        description: "JSON Object",
+        async: true,
+        fn: function __IN__(data, x, source, state, input, output) {
+          var r = function() {
+            var errors = $.editor.validate($.in);
+            if (errors.length) {
+              output({
+                errors: $.create(errors)
+              });
+            } else {
+              output({
+                out: $.get('in')
+              });
+            }
+          }.call(this);
+          return {
+            state: state,
+            return: r
+          };
+        }
+      },
+      editor: {
+        title: "Editor",
+        type: "function",
+        description: "JSONEditor instance"
+      }
+    },
+    output: {
+      out: {
+        title: "Output",
+        type: "string"
+      },
+      errors: {
+        title: "Errors",
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            path: {
+              title: "Path",
+              description: "a dot separated path into the JSON object (e.g. `root.path.to.field`)",
+              type: "string"
+            },
+            property: {
+              title: "Property",
+              description: "schema keyword that triggered the validation error (e.g. `minLength`)",
+              type: "string"
+            },
+            message: {
+              title: "Message",
+              type: "string"
+            }
+          }
+        }
+      }
+    }
+  },
+  state: {}
+}
